ebe69b6df4f3fcb8ba09cf8644cd824a828caf26
Hannah Taubenfeld <hbt3@duke.edu>
2020-01-27 18:20:27 -0500

    stage setup

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -1,13 +1,41 @@
 package cellsociety;
 
+import java.io.IOException;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.util.Duration;
+
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
-public class Main {
-    /**
-     * Start of the program.
-     */
-    public static void main (String[] args) {
-        System.out.println("Hello world");
+public class Main extends Application {
+
+    private static final String TITLE = "Simulation";
+    private static final int FRAMES_PER_SECOND = 60;
+    private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+
+    private Scene myGameScene;
+
+    @Override
+    public void start (Stage stage) {
+        myGameScene = Game.setupSimulation();
+        stage.setScene(myGameScene);
+        stage.setTitle(TITLE);
+        stage.show();
+        KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
+        Timeline animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
     }
+
+    private void step (double elapsedTime) {
+    }
+
 }
