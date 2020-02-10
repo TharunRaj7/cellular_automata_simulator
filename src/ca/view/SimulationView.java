e791e3d53f007dd046e015e65af74a66b86021b9
Hannah Taubenfeld <hbt3@duke.edu>
2020-02-09 20:38:16 -0500

    fire simulation

--- src/ca/view/SimulationView.java
+++ src/ca/view/SimulationView.java
@@ -69,6 +69,9 @@ public class SimulationView {
             case Percolation:
                 simulation = new Percolation(grid);
                 break;
+            case Fire:
+                simulation = new SpreadingOfFire(grid, simulationConfig.getOtherParameters());
+                break;
 //            case WaTorWorld:
 //                simulation = new WaTorWorld(grid, simulationConfig.getOtherParameters());
 //                break;
