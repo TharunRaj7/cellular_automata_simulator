05b5302adf93c69e8cb054afcdfcc1856d22e170
Tharun Raj <tm272@duke.edu>
2020-02-09 19:25:37 -0500

    simulation interaction done

--- src/ca/view/GridPaneHandler.java
+++ src/ca/view/GridPaneHandler.java
@@ -3,6 +3,8 @@ package ca.view;
 
 import ca.controller.SimulationConfig;
 import ca.model.Grid;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
@@ -10,6 +12,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -67,13 +70,28 @@ public class GridPaneHandler {
     private void fillGrid(GridPane myGrid, int c, int r, int cellWidth, int cellHeight, Grid grid){
         for(int i = 0; i< r; i++){
             for(int j = 0; j< c; j++){
+                int finalI = i;
+                int finalJ = j;
                 int cellState = grid.getCellState(i, j);
                 Color cellColor = cellColors.get(cellState);
-                myGrid.add(new Rectangle(cellWidth, cellHeight, cellColor), j, i);
+                Rectangle rect = new Rectangle(cellWidth, cellHeight, cellColor);
+                rect.setOnMouseClicked(event -> {handleClick(rect, grid, finalI, finalJ);});
+                myGrid.add(rect, j, i);
             }
         }
     }
 
+    private void handleClick(Rectangle rect, Grid grid, int i, int j){
+        int oldState = grid.getCellState(i,j);
+        int newState;
+        if (grid.getCellState(i,j) < Collections.max(simulationConfig.getCellStates())){
+            newState = ++oldState;
+        }else{
+            newState = 0;
+        }
+        grid.setCellState(i,j,newState);
+        rect.setFill(cellColors.get(newState));
+    }
     /**
      * From the SimulationConfig, we grab the list of colors that correspond with the initial states and create
      * an array where the index is the state, and the item in the list is the corresponding color.
