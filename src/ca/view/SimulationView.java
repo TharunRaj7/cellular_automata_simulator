db7257dfb6d4be324ee0764069c8b23d80f22229
Cady Zhou <ziqing.zhou@duke.edu>
2020-02-10 01:03:57 -0500

    change to XML chooser

--- src/ca/view/SimulationView.java
+++ src/ca/view/SimulationView.java
@@ -27,8 +27,8 @@ public class SimulationView {
 
     private void attemptOpenXML() {
         try {
-//            simulationConfig = new SimulationConfig(getXMLfile(new Stage()));
-            simulationConfig = new SimulationConfig(new File("data\\Percolation\\Percolation1.xml"), 1, 1);
+            simulationConfig = new SimulationConfig(getXMLfile(new Stage()));
+//            simulationConfig = new SimulationConfig(new File("data\\GameOfLife\\GameOfLife1.xml"), 1, 1);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             attemptOpenXML();
